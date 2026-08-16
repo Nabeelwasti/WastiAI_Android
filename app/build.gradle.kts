3ddaa4b6ef91bd225f75c5c20ef49163d02fff57
Nabeelwasti <125601766+Nabeelwasti@users.noreply.github.com>
2026-08-17 01:46:31 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,18 @@ plugins {
 }
 
 android {
+  ...
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  ...
+}
+
+kotlin {
+  jvmToolchain(17)
+}
+android {
   namespace = "com.example"
   compileSdk = 35
 
